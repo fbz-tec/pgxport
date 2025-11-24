cbd302272123d725b00fb752420e3d6cc15e05e7
Faycal Bououza <faycal.bououza@ratp.fr>
2025-11-24 16:33:37 +0100

    Add automatic buffering for uncompressed file exports

--- core/exporters/compression.go
+++ core/exporters/compression.go
@@ -2,6 +2,7 @@ package exporters
 
 import (
 	"archive/zip"
+	"bufio"
 	"compress/gzip"
 	"fmt"
 	"io"
@@ -18,6 +19,29 @@ const (
 	GZIP = "gzip"
 	ZIP  = "zip"
 )
+
+// bufferedWriteCloser wraps a WriteCloser with buffered I/O
+type bufferedWriteCloser struct {
+	*bufio.Writer
+	underlying io.WriteCloser
+}
+
+// Close flushes the buffer and closes the underlying writer
+func (bwc *bufferedWriteCloser) Close() error {
+	if err := bwc.Writer.Flush(); err != nil {
+		bwc.underlying.Close() // Attempt to close even if flush fails
+		return fmt.Errorf("error flushing buffer: %w", err)
+	}
+	return bwc.underlying.Close()
+}
+
+// newBufferedWriteCloser creates a buffered writer with specified buffer size
+func newBufferedWriteCloser(wc io.WriteCloser, size int) io.WriteCloser {
+	return &bufferedWriteCloser{
+		Writer:     bufio.NewWriterSize(wc, size),
+		underlying: wc,
+	}
+}
 
 type compositeWriteCloser struct {
 	io.Writer
@@ -42,7 +66,8 @@ func createOutputWriter(path string, options ExportOptions, format string) (io.W
 		if err != nil {
 			return nil, fmt.Errorf("error creating file: %w", err)
 		}
-		return file, nil
+		// Using 256KB buffer provides optimal throughput for large exports
+		return newBufferedWriteCloser(file, 256*1024), nil
 
 	case GZIP:
 		if !strings.HasSuffix(strings.ToLower(path), ".gz") {
